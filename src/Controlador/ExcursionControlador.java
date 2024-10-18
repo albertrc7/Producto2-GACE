f840fa89c796a9921c1e68f1983ace20669a30dd
Bassleader777 <gpascualsar@uoc.edu>
2024-10-18 13:38:36 +0200

    Implementació de les classes Vista, Controlador i altres modificacions

--- src/Controlador/ExcursionControlador.java
+++ src/Controlador/ExcursionControlador.java
@@ -1,4 +1,4 @@
-package Controlador;
+/*package Controlador;
 import Modelo.ListaExcursion;
 import Vista.VistaExcursion;
 public class ExcursionControlador {
@@ -20,4 +20,44 @@ public class ExcursionControlador {
     }
 
 }
+
+*/
+package Controlador;
+import Modelo.ListaExcursion;
+import Modelo.Excursion;
+import Vista.VistaExcursion;
+
+public class ExcursionControlador {
+    private ListaExcursion listaExcursion;
+    private VistaExcursion vistaExcursion;
+
+    public ExcursionControlador(ListaExcursion lista, VistaExcursion vista) {
+        this.listaExcursion = lista;
+        this.vistaExcursion = vista;
+    }
+
+    public void gestionarExcursion() {
+        vistaExcursion.mostrarExcursiones(listaExcursion.getListaExcursiones());
+    }
+
+    public void crearExcursion(String codigo, String descripcion, Date fecha, int noDias, double precio) {
+        Excursion excursion = vistaExcursion.formExcursion(codigo, descripcion, fecha, noDias, precio);
+        listaExcursion.anyadirExcursion(excursion);
+    }
+
+    public void eliminarExcursion(String codigo) {
+        Excursion excursion = listaExcursion.getExcursion(codigo);
+        if (excursion != null) {
+            listaExcursion.eliminarExcursion(excursion);
+        }
+    }
+
+    public void mostrarDetalleExcursion(String codigo) {
+        Excursion excursion = listaExcursion.getExcursion(codigo);
+        if (excursion != null) {
+            vistaExcursion.detalleExcursion(excursion);
+        }
+    }
+}
+
 
